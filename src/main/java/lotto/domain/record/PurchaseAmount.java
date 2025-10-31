3c94b2021b364ddb691378799bc51e727cc2fbde
jun <jwjjwj1960@naver.com>
2025-11-01 01:48:01 +0900

    feat: 구입금액에 대한 record 생성 및 유효성 검사
    변하지 않는 구입금액에 대한 불변객체 생성
    구입금액이 0보단 작은지, 1000으로 나누어 떨어지는지 검증

---
+++ src/main/java/lotto/domain/record/PurchaseAmount.java
@@ -0,0 +1,20 @@
+package lotto.domain.record;
+
+import lotto.util.ErrorMessage;
+
+public record PurchaseAmount(int amount) {
+    private static final int LOTTO_PRICE = 1000;
+
+    public PurchaseAmount {
+        if (amount < 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_MIN.getMessage());
+        }
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_POINT.getMessage());
+        }
+    }
+
+    public int getTicketCount() {
+        return this.amount() / LOTTO_PRICE;
+    }
+}
