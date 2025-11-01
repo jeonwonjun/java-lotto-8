d8a493107f120eb5491d93deaa6dd9eb76298201
jun <jwjjwj1960@naver.com>
2025-11-02 00:04:40 +0900

    feat: 로또 번호 중복 및 로또 번호 갯수 에러 메시지 추가
    로또 번호 중복 및 로또 번호 갯수 에러 메시지 추가

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -2,8 +2,10 @@ package lotto.util;
 
 public enum ErrorMessage {
     EMPTY_NUMBER("[ERROR] 로또 번호에는 빈칸이 들어올 수 없습니다."),
-    INVALID_NUMBER_LANGE("[ERROR] 로또 번호는 1~45사이의 값이어야 합니다. "),
+    INVALID_NUMBER_RANGE("[ERROR] 로또 번호는 1~45사이의 값이어야 합니다. "),
     INVALID_NUMBER_FORMAT("[ERROR] 로또 번호는 숫자만 들어와야 합니다."),
+    INVALID_NUMBER_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    INVALID_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
     INVALID_PURCHASE_AMOUN_FORMAT("[ERROR] 구입 금액은 숫자여야 합니다."),
     INVALID_PURCHASE_AMOUNT_POINT("[ERROR] 구입 금액은 1000단위어야 합니다."),
     INVALID_PURCHASE_AMOUNT_MIN("[ERROR] 구입 금액은 0원 이상이어야 합니다.");
