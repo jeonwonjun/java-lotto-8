727f09ba7c86d5a443deb918ba6f1e1d838edad8
jun <jwjjwj1960@naver.com>
2025-11-02 13:05:18 +0900

    feat: BonusNumberParser 추가
    String으로 입력되는 BonusNumber을 Integer로 바꿔준다.
    추가로 숫자로 들어오는지 검증한다.

---
+++ src/main/java/lotto/util/BonusNumberParser.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+import lotto.config.ErrorMessage;
+
+public class BonusNumberParser {
+    public static int parse(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
+        }
+    }
+}
