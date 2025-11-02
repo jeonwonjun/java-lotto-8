854283d3da3f6521473c1536af2e1bad87430b63
jun <jwjjwj1960@naver.com>
2025-11-03 03:01:37 +0900

    feat: 로또묶음 생성 기능 및 당첨집계 기능 추가
    LottoController: issueLottoTickets메서드로 LottoTickets에 로또들 저장,
    aggregateLottoResults로 당첨 집계

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,63 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.config.LottoRank;
+import lotto.domain.entity.BonusNumber;
+import lotto.domain.entity.Lotto;
+import lotto.domain.entity.LottoResults;
+import lotto.domain.entity.LottoTickets;
+import lotto.domain.entity.WinningNumbers;
+import lotto.domain.record.PurchaseAmount;
+import lotto.util.PrizeCalculator;
+import lotto.util.RandomNumbers;
+
 public class LottoController {
 
     public void start() {
-        int purchaseAmount = ViewController.inputPurchaseAmount();
+        PurchaseAmount purchaseAmount = new PurchaseAmount(ViewController.inputPurchaseAmount());
+        int ticketCount = purchaseAmount.getTicketCount();
+        ViewController.printTicketCount(ticketCount);
 
-        int BonusNumber = ViewController.inputBonusNumber();
+        LottoTickets lottoTickets = issueLottoTickets(ticketCount);
+
+        Lotto winningNumber = new Lotto(ViewController.inputWinningNumbers());
+        BonusNumber bonusNumber = new BonusNumber(ViewController.inputBonusNumber(), winningNumber.getNumbers());
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumber.getNumbers(), bonusNumber);
+
+        LottoResults lottoResults = aggregateLottoResults(lottoTickets, winningNumbers);
+
+        double totalResult = PrizeCalculator.calculate(lottoResults.getRankCounts());
+        ViewController.printResults(lottoResults.getRankCounts());
+        ViewController.printProfit(totalResult, (double) purchaseAmount.amount());
+    }
+
+    private LottoTickets issueLottoTickets(int ticketCount) {
+        LottoTickets lottoTickets = new LottoTickets();
+
+        for (int i = 0; i < ticketCount; i++) {
+            RandomNumbers randomNumbers = new RandomNumbers();
+
+            ViewController.printTicket(randomNumbers.getLottoTicket());
+
+            lottoTickets.addTicket(randomNumbers);
+        }
+
+        System.out.println();
+
+        return lottoTickets;
+    }
+
+    private LottoResults aggregateLottoResults(LottoTickets lottoTickets, WinningNumbers winningNumbers) {
+        LottoResults lottoResults = new LottoResults();
+
+        for (RandomNumbers randomNumbers : lottoTickets.getTickets()) {
+            int matchCount = randomNumbers.countMatch(winningNumbers);
+            boolean matchBonus = randomNumbers.matchBonusNumber(winningNumbers);
+
+            LottoRank rank = LottoResults.findRankByMatchBonus(matchCount, matchBonus);
+            lottoResults.addResult(rank);
+        }
+
+        return lottoResults;
     }
 }
--- src/main/java/lotto/controller/ViewController.java
+++ src/main/java/lotto/controller/ViewController.java
@@ -1,6 +1,12 @@
 package lotto.controller;
 
 import java.util.List;
+import java.util.Map;
+import lotto.config.LottoRank;
+import lotto.domain.entity.LottoResults;
+import lotto.domain.entity.WinningNumbers;
+import lotto.util.ProfitRateCalculator;
+import lotto.util.RandomNumbers;
 import lotto.util.parser.BonusNumberParser;
 import lotto.util.parser.PurchaseAmountParser;
 import lotto.util.parser.WinningNumbersParser;
@@ -10,20 +16,36 @@ import lotto.view.OutputView;
 public class ViewController {
     public static int inputPurchaseAmount() {
         String input = InputView.readPurchaseAmount();
+        System.out.println();
         return PurchaseAmountParser.parse(input);
     }
 
     public static List<Integer> inputWinningNumbers() {
         String input = InputView.readWinningNumber();
+        System.out.println();
         return WinningNumbersParser.parse(input);
     }
 
     public static int inputBonusNumber() {
         String input = InputView.readBonusNumber();
+        System.out.println();
         return BonusNumberParser.parse(input);
     }
 
-    public static void printTickets(int ticketCount) {
+    public static void printTicketCount(int ticketCount) {
         OutputView.printTicketCount(ticketCount);
+    }
+
+    public static void printTicket(List<Integer> randomNumbers) {
+        OutputView.printLottoTicket(randomNumbers);
+    }
+
+    public static void printResults(Map<LottoRank, Integer> rankCounts) {
+        OutputView.printWinningResults(rankCounts);
+    }
+
+    public static void printProfit(double purchaseAmount, double totalPrize) {
+        double profitRate = ProfitRateCalculator.calculate(purchaseAmount, totalPrize);
+        OutputView.printProfitRate(profitRate);
     }
 }
