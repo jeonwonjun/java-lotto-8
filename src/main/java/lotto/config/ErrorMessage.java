c7c57b8488282e4684736b188dd723fb70eda7cd
jun <jwjjwj1960@naver.com>
2025-11-03 05:42:52 +0900

    style: [ERROR] 분리
    기존 [ERROR]가 포함되어 있는 메시지를 분리해서 출력할 때 합치기

--- src/main/java/lotto/config/ErrorMessage.java
+++ src/main/java/lotto/config/ErrorMessage.java
@@ -1,22 +1,23 @@
 package lotto.config;
 
 public enum ErrorMessage {
-    EMPTY_NUMBER("[ERROR] 로또 번호에는 공백이 들어올 수 없습니다."),
-    INVALID_NUMBER_RANGE("[ERROR] 로또 번호는 1~45사이의 값이어야 합니다. "),
-    INVALID_NUMBER_FORMAT("[ERROR] 로또 번호는 숫자만 들어와야 합니다."),
-    INVALID_NUMBER_DUPLICATE("[ERROR] 로또 번호는 중복될 수 없습니다."),
-    INVALID_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
-    INVALID_PURCHASE_AMOUNT_FORMAT("[ERROR] 구입 금액은 숫자여야 합니다."),
-    INVALID_PURCHASE_AMOUNT_POINT("[ERROR] 구입 금액은 1000단위어야 합니다."),
-    INVALID_PURCHASE_AMOUNT_MIN("[ERROR] 구입 금액은 1000원 이상이어야 합니다.");
+    EMPTY_NUMBER("로또 번호에는 공백이 들어올 수 없습니다."),
+    INVALID_NUMBER_RANGE("로또 번호는 1~45사이의 값이어야 합니다. "),
+    INVALID_NUMBER_FORMAT("로또 번호는 숫자만 들어와야 합니다."),
+    INVALID_NUMBER_DUPLICATE("로또 번호는 중복될 수 없습니다."),
+    INVALID_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+    INVALID_PURCHASE_AMOUNT_FORMAT("구입 금액은 숫자여야 합니다."),
+    INVALID_PURCHASE_AMOUNT_POINT("구입 금액은 1000단위어야 합니다."),
+    INVALID_PURCHASE_AMOUNT_MIN("구입 금액은 1000원 이상이어야 합니다.");
 
     private final String message;
+    private static final String EORRO_MESSAGE = "[ERROR] %s";
 
     ErrorMessage(String message) {
         this.message = message;
     }
 
     public String getMessage() {
-        return this.message;
+        return String.format(EORRO_MESSAGE, message);
     }
 }
